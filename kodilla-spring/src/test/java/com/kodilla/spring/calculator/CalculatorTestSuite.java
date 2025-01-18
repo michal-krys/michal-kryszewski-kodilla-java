07883b416235df18ac0f3c7daad5dee9de360996
Michal Kryszewski <m.kryszewski@gmail.com>
2025-01-18 09:39:20 +0100

    13.2 kodilla-spring tests corrected

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -4,20 +4,25 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 public class CalculatorTestSuite {
 
     @Autowired
-    private Display display;
+    private Calculator calculator;
 
     @Test
     void testCalculations() {
-        //given
         //when
-        display.displayValue(4.09+3.32);
-        display.displayValue(7-2.65);
-        display.displayValue(5.03*6.22);
-        display.displayValue(12.1/5.3);
+        double val1 = calculator.add(2, 1);
+        double val2 = calculator.sub(3,1);
+        double val3 = calculator.mul(2,3);
+        double val4 = calculator.div(6, 2);
         //then
+        assertEquals(3, val1);
+        assertEquals(2, val2);
+        assertEquals(6, val3);
+        assertEquals(3, val4);
     }
 }
